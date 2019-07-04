3cee00c899115bd901930aedd0787cae1609d27d
Sagar Acharya <sagar_acharya@live.in>
2019-07-04 19:59:45 +0530

    side drawer, navigation and responsiveness for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from'../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <div> Toolbar, SideDrawer, BackDrop</div>
-        <main className={ classes.Content }> 
-            { props.children }
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer:false});
+    }
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={ classes.Content }> 
+                    { this.props.children }
+                </main>
+            </Aux>
+        );
+    }
+}
 
-export default layout;
+export default Layout;
